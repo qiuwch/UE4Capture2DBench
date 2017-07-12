301d6d2b5e4848604e798d9d383d923d994fb0e0
Weichao Qiu <qiuwch@gmail.com>
2017-07-12 14:45:20 -0400

    Initial commit.

---
+++ Source/UE4Capture2DBench/UE4Capture2DBenchGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4Capture2DBench.h"
+#include "UE4Capture2DBenchGameModeBase.h"
+
+
+
+
---
+++ Source/UE4Capture2DBench/UE4Capture2DBenchGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UE4Capture2DBenchGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4CAPTURE2DBENCH_API AUE4Capture2DBenchGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
